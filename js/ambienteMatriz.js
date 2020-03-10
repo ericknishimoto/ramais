494c71bf17f9f766248fc8056e01b14a6a8dd62c
Erick Nishimoto <erick.nishimoto@gmail.com>
2020-03-10 13:14:40 -0300

    Update ambienteMatriz.js

--- js/ambienteMatriz.js
+++ js/ambienteMatriz.js
@@ -1,3 +1,3 @@
-const key = "$2a$10$RMCo1uUgCJKiU.CEXpAZbutR2tOFDnFUdyMY9hUKWU8zajL9EAeHO";
-const jsonDepartamentos = "https://api.jsonbin.io/b/5d122d98f467d60d75a862ec/1";
-const jsonColaboradores = "https://api.jsonbin.io/b/5d1254fac8516d4ebcbb6ab0";
+const key = "$2b$10$s0DS4gyzGoHqHv3oMAyj1evySq3bROQavzqheiJxp2vfQFLQXQUT2";
+const jsonDepartamentos = "https://api.jsonbin.io/b/5e67bcd818ca1e372e49d318";
+const jsonColaboradores = "https://api.jsonbin.io/b/5e67bc5018ca1e372e49d2d2";
